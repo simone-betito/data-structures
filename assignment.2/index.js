192d9043452034df4606db7a66bf73d2fd870f65
Simone <betis855@newschool.com>
2018-09-27 23:30:13 -0400

    JSON file added

--- assignment.2/index.js
+++ assignment.2/index.js
@@ -1,4 +1,4 @@
-var fs = require('fs');
+var fs = require('fs'); // fs = file system module
 var cheerio = require('cheerio');
 
 // load the thesis text file into a variable, `content`
@@ -9,7 +9,7 @@ var cheerio = require('cheerio');
     //xmlMode: true
 
 // this is the file that we created in the starter code from last week
-var content = fs.readFileSync('m05.txt');
+var content = fs.readFileSync('m05.txt'); // .. <- goes back into a level
 
 // load `content` into a cheerio object
 var $ = cheerio.load(content);
@@ -17,20 +17,25 @@ var finalAddress = [];
 var Addresses;
 
 // print (to the console) names of thesis students
-$('td').each(function(i, elem) {
-  if ($(elem).attr("style")  == "border-bottom:1px solid #e3e3e3; width:260px") {
-  Addresses = $(elem).html().split('<br>')[2].trim().split(',')[0]; //+"/n"
-  console.log(Addresses);
+$('td').each(function(i, elem) { // td guarantee way to get to addresses
+
+  if ($(elem).attr("style")  == "border-bottom:1px solid #e3e3e3; width:260px") { //styling being unique to table containing addresses
+  Addresses = $(elem).html().split('<br>')[2].trim().split(',')[0]; //+"\n" <- adds new line marker
+  //let cleanData = Addresses.substring(0, Addresses.indexOf(","))
+  //console.log(Addresses);
   finalAddress.push(Addresses);
+
   }
 });
+//JSON.stringify to turn file into JSON
+//fs.writeFileSync("m05.txt", JSON.stringify(m05Addresses.txt))
 
+console.log(finalAddress); // node on terminal to print
+fs.writeFileSync("finalAddress.json", JSON.stringify(finalAddress));
+// fs.writeFileSync("m05Addresses.txt", JSON.stringify(finalAddress));
 
-console.log(finalAddress);
-fs.writeFileSync("m05Addresses.txt", finalAddress);
 
 // write the project titles to a text file
-//var Addresses = $(elem).html().split('<br>')[2].trim().split(',')[0] + ',' + $(elem).html().split('<br>')[2].trim().split(',')[1] + ','+ '\n' + $(elem).html().split('<br>')[3].trim().split(',')[0] +'\n'+'\n';
 // finalAddress.push(Addresses);
 // console.log(finalAddress);
 //fs.writeFileSync("m05.txt", finalAddress);
